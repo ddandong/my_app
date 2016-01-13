7b629566c3d329cf19e9bf56cc143369aba1cb5b
ddandong <demode@naver.com>
2016-01-13 17:41:36 +0900

    ejs

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static(path.join(__dirname ,'/public')));
+app.set("view engine",'ejs');
+app.use(express.static(path.join(__dirname ,'public')));
+
+app.get('/',function(){
+  res.render('my_first_ejs');
+});
 
 app.listen(3000,function(){
   console.log('Sever is On!');
