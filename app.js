ff6be63b65e40bf01e696d43c0bb1e460e6a235d
ddandong <demode@naver.com>
2016-01-13 18:14:32 +0900

    dynamic

--- app.js
+++ app.js
@@ -5,8 +5,21 @@ var app = express();
 app.set("view engine",'ejs');
 app.use(express.static(path.join(__dirname ,'public')));
 
-app.get('/',function(){
-  res.render('my_first_ejs');
+var data ={count:0};
+app.get('/',function(req,res){
+  data.count++;
+  res.render('my_first_ejs',data);
+});
+
+app.get('/reset',function(req,res){
+  data.count=0;
+  res.render('my_first_ejs',data);
+});
+
+app.get('/set/count',function(req,res){
+    if(req.query.count)
+    data.count = req.query.count;
+    res.render('my_first_ejs',data);
 });
 
 app.listen(3000,function(){
